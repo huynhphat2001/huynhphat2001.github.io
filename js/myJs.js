016bd47c2f54f8c4b23f71ec05e2bedf536df86e
huynhphat2001 <88864093+huynhphat2001@users.noreply.github.com>
2021-08-13 09:19:08 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,5 @@
 const textConfig = {
-    text1: "He luu cậu!",
+    text1: "Chào cậu!",
     text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
     text3: "Cậu yêu tớ có phải không nào ._.",
     text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm người yêu tớ rùi đó nha :v",
